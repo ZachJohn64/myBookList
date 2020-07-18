e961c63fc8d9ecb9a55817614f7dab450e20efc3
ZachJohn64 <zachdjohnson64@gmail.com>
2020-07-18 16:20:29 -0400

    Adde4d Validation Messages

--- app.js
+++ app.js
@@ -48,6 +48,27 @@ class UI {
         list.appendChild(row);
 
     }
+
+    static showAlert(message, className) {
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector('.contain');
+        const form = document.querySelector('#book-form');
+        container.insertBefore(div, form)
+    }
+
+    static deleteBook(el) {
+         if(el.classList.contains('delete')) {
+             el.parentElement.parentElement.remove()
+         }
+    }
+
+    static clearFields() {
+        document.querySelector('#title').value = '';
+        document.querySelector('#author').value = '';
+        document.querySelector('#isbn').value = '';
+    } 
 }
 
 // Store Class: Handles Storage
@@ -56,5 +77,30 @@ class UI {
 document.addEventListener('DOMContentLoaded', UI.displayBooks)
 
 // Event: Add a Book
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+    // Prevent Default
+    e.preventDefault();
+    // Get Form Values
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+
+    // Validate
+    if(title === '' || author === '' || isbn === '') {
+        UI.showAlert('Please fill in all fields', 'danger');
+    } else {
+        // Instantiate book
+        const book = new Book(title, author, isbn);
+
+        // Add book to UI
+        UI.addBookToList(book);
+
+        // Clear Fields
+        UI.clearFields();
+    }
+});
 
 // Event: Remove a Book
+document.querySelector('#book-list').addEventListener('click', (e) => {
+    UI.deleteBook(e.target)
+})
